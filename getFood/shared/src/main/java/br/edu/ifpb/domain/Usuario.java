0e77cf16978d89e1afbc0e8ef42a65151e6ba07b
antonio <antoniofilhonerde@gmail.com>
2019-07-23 16:20:18 -0300

    modelagem de usuario

--- getFood/shared/src/main/java/br/edu/ifpb/domain/Usuario.java
+++ getFood/shared/src/main/java/br/edu/ifpb/domain/Usuario.java
@@ -1,18 +1,27 @@
 package br.edu.ifpb.domain;
 
-public class Usuario {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
 
+@Entity
+public class Usuario implements Serializable {
+
+    @Id
     private String cpf;
     private String email;
     private String nome;
+    private String telefone;
 
     public Usuario() {
     }
 
-    public Usuario(String cpf, String email, String nome) {
+    public Usuario(String cpf, String email, String nome, String telefone) {
         this.cpf = cpf;
         this.email = email;
         this.nome = nome;
+        this.telefone = telefone;
     }
 
     public String getCpf() {
@@ -38,4 +47,29 @@ public class Usuario {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(cpf, usuario.cpf) &&
+                Objects.equals(email, usuario.email) &&
+                Objects.equals(nome, usuario.nome) &&
+                Objects.equals(telefone, usuario.telefone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf, email, nome, telefone);
+    }
 }
+
