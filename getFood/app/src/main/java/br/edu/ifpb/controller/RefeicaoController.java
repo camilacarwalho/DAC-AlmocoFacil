c24d449ccecf027e8c2caf55b717a3b08d386f9f
isleimar <isleimar@gmail.com>
2019-08-03 20:41:02 -0300

    Criação da Classe RefeicaoController

---
+++ getFood/app/src/main/java/br/edu/ifpb/controller/RefeicaoController.java
@@ -0,0 +1,25 @@
+package br.edu.ifpb.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.edu.ifpb.dao.RefeicaoDao;
+import br.edu.ifpb.domain.Refeicao;
+
+@SuppressWarnings("serial")
+@RequestScoped
+@Named
+public class RefeicaoController implements Serializable{
+	
+	@Inject
+	private RefeicaoDao refeicaoDao;
+	
+	public List<Refeicao> getListaDeRefeicoes(){
+		return refeicaoDao.listar();
+	}
+
+}
