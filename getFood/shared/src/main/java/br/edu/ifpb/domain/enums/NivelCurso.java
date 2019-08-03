dc6e6f80b70e421eaad45e8eb889c72ae7ae5cf5
isleimar <isleimar@gmail.com>
2019-08-03 10:45:17 -0300

    Incluíndo o nome no ENUM NivelCruso

--- getFood/shared/src/main/java/br/edu/ifpb/domain/enums/NivelCurso.java
+++ getFood/shared/src/main/java/br/edu/ifpb/domain/enums/NivelCurso.java
@@ -1,8 +1,14 @@
 package br.edu.ifpb.domain.enums;
 
 public enum NivelCurso {
-    INTEGRADO,
-    SUBSEQUENTE,
-    GRADUACAO,
-    POSGRADUACAO
+    INTEGRADO("Integrado"),
+    SUBSEQUENTE("Subserquente"),
+    GRADUACAO("Graduação"),
+    POSGRADUACAO("Pós-graduação");
+	
+	private String nome;
+	
+	private NivelCurso(String nome) {this.nome = nome;}
+	public String getNome() {return nome;}
+	
 }
