731fa4200d471db3fae615ff5ec580b3b1f934f8
antonio <antoniofilhonerde@gmail.com>
2019-08-03 20:51:17 -0300

    extendendo defaultdao para usuario

--- getFood/shared/src/main/java/br/edu/ifpb/dao/UsuarioDao.java
+++ getFood/shared/src/main/java/br/edu/ifpb/dao/UsuarioDao.java
@@ -4,14 +4,7 @@ import br.edu.ifpb.domain.Usuario;
 
 import java.util.List;
 
-public interface UsuarioDao {
-
-    void salvar(Usuario usuario);
-    void remover(Usuario usuario);
-    List<Usuario> listar();
-    void atualizar(Usuario usuario);
-    Usuario getById(String matricula);
-
+public interface UsuarioDao extends DefaultDao<Usuario> {
 
 
 }
