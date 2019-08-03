a1807d4e88d5393486be441dd1e3d6e214c3f7a6
isleimar <isleimar@gmail.com>
2019-08-03 10:54:59 -0300

    Implementação da Classe Refeicao

---
+++ getFood/shared/src/main/java/br/edu/ifpb/domain/Refeicao.java
@@ -0,0 +1,84 @@
+package br.edu.ifpb.domain;
+
+import java.time.LocalTime;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Refeicao {
+	
+	@Id
+	private int id;
+	private String nome;
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalTime horaInicio;
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalTime horaTermino;
+	
+	public Refeicao() {	}
+
+	public Refeicao(int id, String nome, LocalTime horaInicio, LocalTime horaTermino) {
+		this.id = id;
+		this.nome = nome;
+		this.horaInicio = horaInicio;
+		this.horaTermino = horaTermino;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalTime getHoraInicio() {
+		return horaInicio;
+	}
+
+	public void setHoraInicio(LocalTime horaInicio) {
+		this.horaInicio = horaInicio;
+	}
+
+	public LocalTime getHoraTermino() {
+		return horaTermino;
+	}
+
+	public void setHoraTermino(LocalTime horaTermino) {
+		this.horaTermino = horaTermino;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Refeicao other = (Refeicao) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
