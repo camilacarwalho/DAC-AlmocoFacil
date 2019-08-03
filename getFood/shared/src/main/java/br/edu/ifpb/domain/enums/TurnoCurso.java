3d27f93d0d52ac39925b9cbae02b74c97b8094bf
isleimar <isleimar@gmail.com>
2019-08-03 14:28:45 -0300

    Incluíndo o nome no ENUM TurnoCurso

--- getFood/shared/src/main/java/br/edu/ifpb/domain/enums/TurnoCurso.java
+++ getFood/shared/src/main/java/br/edu/ifpb/domain/enums/TurnoCurso.java
@@ -2,6 +2,9 @@ package br.edu.ifpb.domain.enums;
 
 public enum TurnoCurso {
 
-    INTEGRAL,
-    NOTURNO
+    INTEGRAL("Integral"),
+    NOTURNO("Noturno");
+	private String nome;
+	private TurnoCurso(String nome) {this.nome = nome;}
+	public String getNome() {return nome;}
 }
