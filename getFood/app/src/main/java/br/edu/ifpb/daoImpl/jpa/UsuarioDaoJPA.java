031ea70089cbf92b5883214b3e05fb0e622f3406
antonio <antoniofilhonerde@gmail.com>
2019-08-04 20:38:21 -0300

    stateless usuariodao

--- getFood/app/src/main/java/br/edu/ifpb/daoImpl/jpa/UsuarioDaoJPA.java
+++ getFood/app/src/main/java/br/edu/ifpb/daoImpl/jpa/UsuarioDaoJPA.java
@@ -3,90 +3,46 @@ package br.edu.ifpb.daoImpl.jpa;
 import br.edu.ifpb.dao.UsuarioDao;
 import br.edu.ifpb.domain.Usuario;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.ejb.Stateless;
+import javax.persistence.*;
 import java.util.Collections;
 import java.util.List;
 
+@Stateless
 public class UsuarioDaoJPA implements UsuarioDao {
 
+    @PersistenceContext
     private EntityManager em;
-    private EntityTransaction tr;
-
-    public UsuarioDaoJPA(){
-        this.em = Persistence
-                .createEntityManagerFactory("Postgres")
-                .createEntityManager();
-        this.tr=em.getTransaction();
-    }
 
 
     @Override
     public void salvar(Usuario usuario) {
-        try{
-            tr.begin();
             em.persist(usuario);
-            tr.commit();
-        }catch (Exception ex){
-            ex.printStackTrace();
-            tr.rollback();
-        }
-
     }
 
     @Override
     public void remover(Usuario usuario) {
-        try{
-            tr.begin();
             Usuario morto = buscar(usuario.getMatricula());
             em.remove(morto);
-            tr.commit();
-        }catch (Exception ex){
-            ex.printStackTrace();
-            tr.rollback();
-        }
     }
 
     @Override
     public List<Usuario> listar() {
-        try{
-            tr.begin();
-            List<Usuario> usuarios = em
-                    .createQuery("SELECT u FROM Usuario u")
-                    .getResultList();
-            tr.commit();
-            return usuarios;
-        }catch (Exception ex){
-            ex.printStackTrace();
-            tr.rollback();
-            return Collections.emptyList();
-        }
+        String jpql = "SELECT u FROM Usuario u";
+        TypedQuery<Usuario> query = em.createQuery(jpql, Usuario.class);
+        return query.getResultList();
     }
 
     @Override
     public void atualizar(Usuario usuario) {
-        try{
-            tr.begin();
-            em.merge(usuario);
-            tr.commit();
-        }catch (Exception ex){
-            ex.printStackTrace();
-            tr.rollback();
-        }
+        em.merge(usuario);
     }
 
     @Override
     public Usuario buscar(Object matricula) {
-        try{
-            tr.begin();
-            Usuario encontrado = em.find(Usuario.class,matricula);
-            tr.commit();
-            return encontrado;
-        }catch (Exception ex){
-            ex.printStackTrace();
-            tr.rollback();
-            return null;
-        }
+        Usuario encontrado = new Usuario();
+        encontrado = em.find(Usuario.class,matricula);
+        return encontrado;
+
     }
 }
