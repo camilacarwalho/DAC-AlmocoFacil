65048290383f005412b62649ec2265aaadee46ad
antonio <antoniofilhonerde@gmail.com>
2019-08-04 20:38:40 -0300

    iniciando controller de usuario

--- getFood/app/src/main/java/br/edu/ifpb/controller/UsuarioController.java
+++ getFood/app/src/main/java/br/edu/ifpb/controller/UsuarioController.java
@@ -1,4 +1,23 @@
 package br.edu.ifpb.controller;
 
-public class UsuarioController {
+import br.edu.ifpb.dao.UsuarioDao;
+import br.edu.ifpb.domain.Usuario;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@RequestScoped
+@Named
+public class UsuarioController implements Serializable {
+
+    @Inject
+    private UsuarioDao usuarioDao;
+
+    public List<Usuario> getListaDeUsuarios(){
+        return usuarioDao.listar();
+    }
+
 }
