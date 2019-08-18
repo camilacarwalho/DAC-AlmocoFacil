3c40198c582733ec944aa3b949ed1f4d5ae95998
isleimar <isleimar@gmail.com>
2019-08-18 19:38:54 -0300

    Refatorando o UsuarioEnum

--- getFood/shared/src/main/java/br/edu/ifpb/domain/enums/UsuarioEnum.java
+++ getFood/shared/src/main/java/br/edu/ifpb/domain/enums/UsuarioEnum.java
@@ -1,8 +1,21 @@
 package br.edu.ifpb.domain.enums;
 
 public enum UsuarioEnum {
-    PROFESSOR,
-    CAEST,
-    GESTOR,
-    ALUNO
+	DEFAULT("Sem usuário conectado","default"),
+    PROFESSOR("Professor","professor"),
+    CAEST("Gestor da CAEST","caest"),
+    GESTOR("Gestor Administrativo","gestor"),
+    ALUNO("Aluno","aluno"),
+    REFEITORIO("Refeitório","refeitorio");
+    private String nome;
+	private String identificador;
+	private UsuarioEnum(String nome, String identificador) {
+		this.nome = nome;
+		this.identificador = identificador;
+	}
+	public String getNome() {return nome;}
+	public String getIdentificador() {return identificador;}
+	
+    
+    
 }
