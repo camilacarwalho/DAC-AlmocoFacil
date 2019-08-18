cf55135cadba394599aa0199c53e4413f943903b
isleimar <isleimar@gmail.com>
2019-08-18 18:47:38 -0300

    Implementando o MensagensAlert

--- getFood/app/src/main/java/br/edu/ifpb/service/MensagensAlert.java
+++ getFood/app/src/main/java/br/edu/ifpb/service/MensagensAlert.java
@@ -1,5 +1,33 @@
 package br.edu.ifpb.service;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
 public class MensagensAlert {
+	
+	public static void addInfoMessage(String msg) {
+		FacesMessage infoMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, 
+	            "Informação: ", msg);
+	        FacesContext.getCurrentInstance().addMessage(null, infoMessage);
+	}
+	
+	public static void addWarningMessage(String msg) {
+		FacesMessage warningMessage = new FacesMessage(FacesMessage.SEVERITY_WARN, 
+				"Atenção: ", msg);
+	        FacesContext.getCurrentInstance().addMessage(null, warningMessage);
+	}
+	
+	public static void addErrorMessage(String msg) {
+		FacesMessage errorMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, 
+				"Erro: ", msg);
+	        FacesContext.getCurrentInstance().addMessage(null, errorMessage);
+	}
+	
+	public static void addFatalMessage(String msg) {
+		FacesMessage fatalMessage = new FacesMessage(FacesMessage.SEVERITY_FATAL, 
+				"Fatal: ", msg);
+	        FacesContext.getCurrentInstance().addMessage(null, fatalMessage);
+	}
+	
 
 }
