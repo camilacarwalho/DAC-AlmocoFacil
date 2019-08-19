e3bdc150620a50aeae431373eb4636d5bba95e73
isleimar <isleimar@gmail.com>
2019-08-18 22:19:50 -0300

    Implementada a classe DadosInvalidoException

---
+++ getFood/shared/src/main/java/br/edu/ifpb/exceptions/DadosInvalidosException.java
@@ -0,0 +1,14 @@
+package br.edu.ifpb.exceptions;
+
+public class DadosInvalidosException  extends Exception{
+
+	public DadosInvalidosException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+	
+
+}
