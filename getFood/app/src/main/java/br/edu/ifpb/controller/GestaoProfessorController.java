1881a46890b7632f3d981445b81c126ab0a8903f
antonio <antoniofilhonerde@gmail.com>
2019-08-28 09:07:06 -0300

    controlador de gestao de professores

--- getFood/app/src/main/java/br/edu/ifpb/controller/GestaoProfessorController.java
+++ getFood/app/src/main/java/br/edu/ifpb/controller/GestaoProfessorController.java
@@ -1,4 +1,34 @@
 package br.edu.ifpb.controller;
 
-public class GestaoProfessorController {
+import br.edu.ifpb.domain.Usuario;
+import br.edu.ifpb.service.UsuarioService;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@ViewScoped
+@Named
+public class GestaoProfessorController extends PaginacaoController<Usuario> implements Serializable {
+
+    @Inject
+    UsuarioService usuarioService;
+
+    @PostConstruct
+    public void init(){
+        buscar();
+    }
+
+    @Override
+    protected List<Usuario> listarItensDaBusca(int inicio, int maximo) {
+        return usuarioService.buscarProfessores(inicio,maximo);
+    }
+
+    @Override
+    public int getQuantidadeItens() {
+        return usuarioService.quantProfessores();
+    }
 }
