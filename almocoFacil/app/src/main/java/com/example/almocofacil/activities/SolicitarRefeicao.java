d92fbf1bc68b33aefdf62e5dba6550dac09d0ca6
KaiqueSousa <kaique.una@gmail.com>
2019-09-06 19:59:11 -0300

    Tela Solicitar Refeição

---
+++ almocoFacil/app/src/main/java/com/example/almocofacil/activities/SolicitarRefeicao.java
@@ -0,0 +1,16 @@
+package com.example.almocofacil.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.almocofacil.R;
+
+public class SolicitarRefeicao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitar_refeicao);
+    }
+}
