7f0f93e9e1f00d7017359ba0151e9bcffbdd4eee
isleimar <isleimar@gmail.com>
2019-09-12 23:01:47 -0300

    Corrigindo o Refeição Controller

--- getFood/app/src/main/java/br/edu/ifpb/controller/RefeicaoController.java
+++ getFood/app/src/main/java/br/edu/ifpb/controller/RefeicaoController.java
@@ -1,6 +1,7 @@
 package br.edu.ifpb.controller;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
@@ -10,48 +11,69 @@ import javax.inject.Named;
 import br.edu.ifpb.domain.Refeicao;
 import br.edu.ifpb.domain.Requisicao;
 import br.edu.ifpb.service.RefeicaoService;
-import java.time.LocalDate;
 
 @SuppressWarnings("serial")
 @RequestScoped
 @Named
-public abstract class RefeicaoController implements Serializable{
+public class RefeicaoController implements Serializable {
 
 	@Inject
-	private RefeicaoService service;
-        @Inject
-        private Refeicao refeicao;
-        private String nome;
-        private LocalDate horaInicio;
-        private LocalDate horaTermino;
-        
-        public void salvarRefeicao(){
-            service.salvar(refeicao);
-        }
-        
-        public void atualizarRefeicao() {
-            service.atualizar(getRefeicao());
-        }
-        
-        public void removerRefeicao(){
-            service.remover(refeicao);
-        }
-	
-	public List<Refeicao> getListaDeRefeicoes(){
-            return service.listar();
+	private RefeicaoService service;	
+	private Refeicao refeicao;
+	private String nome;
+	private LocalDate horaInicio;
+	private LocalDate horaTermino;
+
+	public void salvarRefeicao() {
+		service.salvar(refeicao);
 	}
-        
-        public List<Requisicao> getRefeicoesNoDia(){
-            return service.refeicoesNoDia(LocalDate.now());
-        }
-        
-        public Refeicao getRefeicao() {return refeicao;}
-	public void setRefeicao(Refeicao refeicao) {this.refeicao = refeicao;}
-        public String getNome() { return nome; }
-        public void setNome(String nome) { this.nome = nome; }
-        public LocalDate getHoraInicio() { return horaInicio; }
-        public void setHoraInicio(LocalDate horaInicio) { this.horaInicio = horaInicio; }
-        public LocalDate getHoraTermino() { return horaTermino; }
-        public void setHoraTermino(LocalDate horaTermino) { this.horaTermino = horaTermino; }
-        
+
+	public void atualizarRefeicao() {
+		service.atualizar(getRefeicao());
+	}
+
+	public void removerRefeicao() {
+		service.remover(refeicao);
+	}
+
+	public List<Refeicao> getListaDeRefeicoes() {
+		return service.listar();
+	}
+
+	public List<Requisicao> getRefeicoesNoDia() {
+		return service.refeicoesNoDia(LocalDate.now());
+	}
+
+	public Refeicao getRefeicao() {
+		return refeicao;
+	}
+
+	public void setRefeicao(Refeicao refeicao) {
+		this.refeicao = refeicao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getHoraInicio() {
+		return horaInicio;
+	}
+
+	public void setHoraInicio(LocalDate horaInicio) {
+		this.horaInicio = horaInicio;
+	}
+
+	public LocalDate getHoraTermino() {
+		return horaTermino;
+	}
+
+	public void setHoraTermino(LocalDate horaTermino) {
+		this.horaTermino = horaTermino;
+	}
+
 }
