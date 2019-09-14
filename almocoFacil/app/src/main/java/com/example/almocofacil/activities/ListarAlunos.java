edb2e9696d438932caa44eb367931c7a712c5b43
romuloqaz <romulo.pereiracz@gmail.com>
2019-09-14 10:29:04 -0300

    refatoranto tela refeitorio

--- almocoFacil/app/src/main/java/com/example/almocofacil/activities/ListarAlunos.java
+++ almocoFacil/app/src/main/java/com/example/almocofacil/activities/ListarAlunos.java
@@ -5,52 +5,89 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.almocofacil.R;
 import com.example.almocofacil.domain.Aluno;
 
-public class ListarAlunos extends Activity {
+public class ListarAlunos extends Activity implements AdapterView.OnItemSelectedListener {
 
-    private ArrayList<Aluno> listAluno;
+    private Spinner spinner;
+    ArrayList<String> selectedItems;
 
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainlistaluno);
 
-        ListView lview = (ListView) findViewById(R.id.listview);
-        populateList();
-        ListAlunoAdapter adapter = new ListAlunoAdapter(listAluno, this);
-        lview.setAdapter(adapter);
+        //spinner
+
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this,
+                R.array.lista_refeicao, android.R.layout.simple_spinner_item);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setOnItemSelectedListener(this);
+
+        selectedItems=new ArrayList<String>();
     }
 
-    // TODO: para teste
-    private void populateList() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.logout, menu);
+        return true;
+    }
 
-        listAluno = new ArrayList<Aluno>();
-        Aluno a = new Aluno("123", "Enrico Vicente Elias da Mata");
-        Aluno b = new Aluno("124", "Sebastiana Silvana Nascimento");
-        Aluno c = new Aluno("125", "Maya Ester Pereira");
-        Aluno d = new Aluno("126", "Isabel Clarice Porto");
-        Aluno e = new Aluno("127", "Diogo Miguel Ferreira");
-        Aluno f = new Aluno("128", "Marli Benedita Liz da Mota");
-        Aluno g = new Aluno("129", "Hadassa Flávia Barros");
-        Aluno h = new Aluno("130", "Rodrigo Anderson Lorenzo da Luz");
-        Aluno i = new Aluno("131", "Alana Camila Josefa Lopes");
-        Aluno j = new Aluno("132", "Malu Sara Moraes");
-        Aluno k = new Aluno("133", "Isabelle Valentina Lorena Nogueira");
-        listAluno.add(a);
-        listAluno.add(b);
-        listAluno.add(c);
-        listAluno.add(d);
-        listAluno.add(e);
-        listAluno.add(f);
-        listAluno.add(g);
-        listAluno.add(h);
-        listAluno.add(i);
-        listAluno.add(j);
-        listAluno.add(k);
+    private ArrayList<String> preencherDados() {
+        ArrayList<String> dados = new ArrayList<String>();
+        dados.add("almoço - 06/09/2019 - 07/09/2019");
+        dados.add("almoço - 06/09/2019 - 07/09/2019");
+        dados.add("almoço - 06/09/2019 - 07/09/2019");
+        dados.add("almoço - 06/09/2019 - 07/09/2019");
+        dados.add("almoço - 06/09/2019 - 07/09/2019");
+        dados.add("almoço - 06/09/2019 - 07/09/2019");
+        dados.add("almoço - 06/09/2019 - 07/09/2019");
 
+        return dados;
+    }
+    public void onStart(){
+        super.onStart();
+        ListView lview = (ListView) findViewById(R.id.checkable_list);
+        lview.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        ArrayList<String> aluno = preencherDados();
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_multiple_choice, aluno);
+        lview.setAdapter(arrayAdapter);
+        //set OnItemClickListener
+        lview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // selected item
+                String selectedItem = ((TextView) view).getText().toString();
+                if(selectedItems.contains(selectedItem))
+                    selectedItems.remove(selectedItem); //remove deselected item from the list of selected items
+                else
+                    selectedItems.add(selectedItem); //add selected item to the list of selected items
+            }
+
+        });
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
