9fb74262dc376df77769dfc462a8ad748434cf51
isleimar <isleimar@gmail.com>
2019-09-13 23:09:04 -0300

    Refatorando SolicitacaoActivity

--- almocoFacil/app/src/main/java/com/example/almocofacil/activities/SolicitarRefeicaoActivity.java
+++ almocoFacil/app/src/main/java/com/example/almocofacil/activities/SolicitarRefeicaoActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -24,7 +25,9 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class SolicitarRefeicaoActivity extends AppCompatActivity {
@@ -36,6 +39,7 @@ public class SolicitarRefeicaoActivity extends AppCompatActivity {
     private RadioButton rbAlmoco;
     private RadioButton rbJantar;
     private Button btSolAdicionarEstudante;
+    private Button btSolicitar;
     private ListView lvAlunos;
     private ProgressDialog progress;
 
@@ -63,11 +67,18 @@ public class SolicitarRefeicaoActivity extends AppCompatActivity {
         });
 
         btSolAdicionarEstudante = findViewById(R.id.btSolAdicionarEstudante);
-
         btSolAdicionarEstudante.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 adicionarEstudante();
+            }
+        });
+
+        btSolicitar = findViewById(R.id.btSolicitar);
+        btSolicitar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                solicitar();
             }
         });
 
@@ -78,6 +89,55 @@ public class SolicitarRefeicaoActivity extends AppCompatActivity {
 
         carregarDados();
     }
+
+    private void solicitar() {
+        if (!escreverDados())
+            return;
+        progress.setTitle("Requisitando...");
+        progress.show();
+        new AcessoRest<Requisicao>(
+                this,
+                "requisicao/salvar/",
+                Requisicao.class){
+
+            @Override
+            public void retorno(Requisicao objeto) {
+                if (objeto != null) {
+                    requisicao = objeto;
+                    incluirAlunos();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Não foi possível realizar a solicitação.", Toast.LENGTH_LONG).show();
+                }
+                progress.dismiss();
+            }
+        }.put(requisicao);
+    }
+
+    private void incluirAlunos() {
+        progress.setTitle("Incluíndo alunos...");
+        progress.show();
+        List<String> matriculas = new ArrayList<>();
+        for (Aluno aluno: alunos)
+            matriculas.add(aluno.getMatricula());
+        new AcessoRest<String>(
+                this,
+                "requisicao/listaAlunos/"+ requisicao.getRequisicaoId(),
+                String.class){
+
+            @Override
+            public void retorno(String objeto) {
+                progress.dismiss();
+                if (objeto != null) {
+                    Toast.makeText(getApplicationContext(), "Solicitação realizada.", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(getApplicationContext(), AcompanharSolicitacaoActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Não foi possível realizar a solicitação.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }.put(matriculas);
+    }
+
 
     private void removerAluno(final Aluno aluno) {
 
@@ -117,26 +177,64 @@ public class SolicitarRefeicaoActivity extends AppCompatActivity {
         progress.show();
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         edDescricao.setText(requisicao.getDescricao());
-        edDataInicio.setText(sdf.format(requisicao.getDataInicio()));
-        edDataFinal.setText(sdf.format(requisicao.getDataFinal()));
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(requisicao.getDataInicio());
+        cal.add(Calendar.DAY_OF_MONTH,1);
+        edDataInicio.setText(sdf.format(cal.getTime()));
+
+        cal.setTime(requisicao.getDataFinal());
+        cal.add(Calendar.DAY_OF_MONTH,1);
+        edDataFinal.setText(sdf.format(cal.getTime()));
+
         rbAlmoco.setChecked(requisicao.getRefeicaoId() == 1);
         rbJantar.setChecked(requisicao.getRefeicaoId() == 2);
-        Type listType = new TypeToken<ArrayList<Aluno>>(){}.getType();
-        new AcessoRest<List<Aluno>>(
-                this,
-                "requisicao/listaAlunos/"+ requisicao.getRequisicaoId(),
-                listType){
-            @Override
-            public void retorno(final List<Aluno> objeto) {
-                if (objeto != null) {
-                    alunos = new ArrayList<>();
-                    for (Aluno novoAluno : objeto)
-                        alunos.add(novoAluno);
-                }else
-                    alunos = new ArrayList<>();
-                listarAlunos();
-            }
-        }.get();
+        if(requisicao.getRequisicaoId() != 0) {
+            Type listType = new TypeToken<ArrayList<Aluno>>() {
+            }.getType();
+            new AcessoRest<List<Aluno>>(
+                    this,
+                    "requisicao/listaAlunos/" + requisicao.getRequisicaoId(),
+                    listType) {
+                @Override
+                public void retorno(final List<Aluno> objeto) {
+                    if (objeto != null) {
+                        alunos = new ArrayList<>();
+                        for (Aluno novoAluno : objeto)
+                            alunos.add(novoAluno);
+                    } else
+                        alunos = new ArrayList<>();
+                    listarAlunos();
+                }
+            }.get();
+        } else {
+            progress.dismiss();
+        }
+    }
+
+    private boolean escreverDados() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        requisicao.setDescricao(edDescricao.getText().toString());
+        if((!rbAlmoco.isChecked()) && !(rbJantar.isChecked())){
+            Toast.makeText(getApplicationContext(),"Selecione uma refeição",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if(rbAlmoco.isChecked()){
+            requisicao.setRefeicaoNome("Almoço");
+            requisicao.setRefeicaoId(1);
+        } else {
+            requisicao.setRefeicaoNome("Jantar");
+            requisicao.setRefeicaoId(2);
+        }
+
+        try {
+            requisicao.setDataInicio(sdf.parse(edDataInicio.getText().toString()));
+            requisicao.setDataFinal(sdf.parse(edDataFinal.getText().toString()));
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(),"Data inválida",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
     }
 
     private void listarAlunos() {
